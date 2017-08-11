40afa03cc90cf43d39d06dbca0259d2918dd6642
Jake Pogue <bb2jake@gmail.com>
2017-08-11 16:37:19 -0600

    Clock and greeting working

--- public/app/components/clock/clock-controller.js
+++ public/app/components/clock/clock-controller.js
@@ -2,11 +2,18 @@ function ClockController() {
 	var _this = this;
 	var service = new ClockService();
 
-	function drawClock(time) {
-		
+	function getTime() {
+		service.getTime(drawClock);
+	}
+
+	function drawClock(time, greeting, userName) {
+		$('#clock-text').html(time);
+		$('#greeting-text').html(greeting + userName);
 	}
 
 	this.toggleMilitaryTime = function() {
-
+		service.toggleMilitaryTime(getTime);
 	}
+	
+	service.updateTimer(getTime);
 }
--- public/app/components/clock/clock-service.js
+++ public/app/components/clock/clock-service.js
@@ -1,3 +1,51 @@
-function ClockService () {
+function ClockService() {
 	var _this = this;
+	var isMilitary = false;
+	var userName = "Jake";
+	var shownTime = '';
+
+	this.getTime = function (drawClock) {
+		let date = new Date();
+		let append = '';
+		let hours = date.getHours();
+		let greeting = getGreeting(hours);
+		let minutes = date.getMinutes();
+
+		if (minutes < 10)
+			minutes = "0" + minutes;
+
+		if (!isMilitary) {
+			if (hours >= 12) {
+				hours -= 12;
+				append = 'pm';
+			} else {
+				append = 'am';
+			}
+		}
+
+		let time = hours + ":" + minutes + append;
+		if (time != shownTime) {
+			shownTime = time;
+			drawClock(time, greeting, userName);
+		}
+	}
+
+	function getGreeting(hours) {
+		if (hours < 12)
+			return "Good morning, ";
+		else if (hours < 18)
+			return "Good afternoon, ";
+		else
+			return "Good evening, ";
+	}
+
+	this.toggleMilitaryTime = function (getTime) {
+		isMilitary = !isMilitary;
+		getTime();
+	}
+
+	this.updateTimer = function (getTime) {
+		getTime();
+		setInterval(getTime, 1000);
+	}
 }
